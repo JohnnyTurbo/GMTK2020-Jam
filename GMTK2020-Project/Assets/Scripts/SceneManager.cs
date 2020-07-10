2d056eb5930f4294f547575bbb82ed66579042a8
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-07-10 11:19:41 -0700

    Initial commit with Unity files.

---
+++ GMTK2020-Project/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TMG.GMTK2020
+{
+    public class SceneManager : MonoBehaviour
+    {
+		//private static SceneManager _instance;
+        public static SceneManager instance;
+
+		SceneManager sceneManager;
+
+		private void Awake()
+		{
+			if (instance != null)
+			{
+				Destroy(gameObject);
+			}
+			else
+			{
+				instance = this;
+				DontDestroyOnLoad(this);
+			}
+
+
+		}
+
+		public void LoadNewScene(string sceneName)
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+		}
+	}
+}
