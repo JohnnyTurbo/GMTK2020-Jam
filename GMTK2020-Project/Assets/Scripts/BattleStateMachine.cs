3681cc444fed72e85e61846b9e75a68bcc0fa779
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-07-10 14:14:42 -0700

    Created Battle Scene and beginning of State Machine.

---
+++ GMTK2020-Project/Assets/Scripts/BattleStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TMG.GMTK2020
+{
+    public enum BattleState { PlayerActionSelect, PlayerActionPlayback, EnemyActionSelect, EnemyActionPlayback, None}
+
+    public class BattleStateMachine : MonoBehaviour
+    {
+        public BattleState curBattleState;
+
+        public void BeginBattle()
+		{
+            curBattleState = BattleState.PlayerActionSelect;
+		}
+
+
+    }
+}
