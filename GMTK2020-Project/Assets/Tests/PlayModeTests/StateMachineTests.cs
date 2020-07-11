e80d6376d25a6b7bb1e8b2804bb56f8a6e37a76a
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-07-10 22:59:10 -0700

    Implemented a Play Mode test to verify states are being properly changed when entering battle.

--- GMTK2020-Project/Assets/Tests/PlayModeTests/StateMachineTests.cs
+++ GMTK2020-Project/Assets/Tests/PlayModeTests/StateMachineTests.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using TMG.GMTK2020;
 
 namespace Tests
 {
     public class StateMachineTests
     {
         [UnityTest]
-        public IEnumerator StateMachineTestsWithEnumeratorPasses()
+        public IEnumerator Test_States_Entering_Battle()
         {
-            
+            GameObject battleStateMachineGO = new GameObject();
+            BattleStateMachine battleStateMachine = battleStateMachineGO.AddComponent<BattleStateMachine>();
+
+            GameObject battleControllerGO = new GameObject();
+            BattleController battleController = battleControllerGO.AddComponent<BattleController>();
+
+            Assert.AreEqual(BattleState.None, BattleStateMachine.instance.curBattleState);
+
+            battleController.BeginBattle();
+
+            Assert.AreEqual(BattleState.PreNewBattleSetup, BattleStateMachine.instance.curBattleState);
+
             yield return null;
+
+            Assert.AreEqual(BattleState.NewBattleSetup, BattleStateMachine.instance.curBattleState);
+
+            yield return null;
+
+            Assert.AreEqual(BattleState.PlayerActionSelect, BattleStateMachine.instance.curBattleState);
         }
     }
 }
